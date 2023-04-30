e53d13f2289a54667013e8f12eb8c7ee922195d6
Vinicius Correia <66895634+vinicius-vagner@users.noreply.github.com>
2023-04-29 22:51:59 -0300

    Add files via upload

---
+++ nomeCompleto.rb
@@ -0,0 +1,7 @@
+puts "Digite seu nome: "
+nome = gets.chomp
+puts "Digite seu Sobre nome: "
+sobrenome = gets.chomp
+puts "Digite sua idade: "
+idade = gets.chomp.to_i
+puts "Bem vindo #{nome} #{sobrenome} de #{idade} anos" 
